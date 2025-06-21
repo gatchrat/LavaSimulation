42000d6011f658d22fc0e426f325f93d51579e9b
gatchrat <henkolt@t-online.de>
2025-06-21 20:26:11 +0200

    sceneswitching

---
+++ SCripts/LevelLoader.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    public void LoadSPH2D()
+    {
+        SceneManager.LoadScene("2D");
+    }
+    public void LoadSPH()
+    {
+        SceneManager.LoadScene("SPH");
+    }
+    public void LoadShuriken()
+    {
+        SceneManager.LoadScene("UnityPartikles");
+    }
+}
